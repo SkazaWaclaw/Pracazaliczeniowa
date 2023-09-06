c031bec2ae1493fee1bb204e38690673ec3c0b2a
Skaza Waclaw <skaza.ws@gmail.com>
2023-09-06 21:29:36 +0200

    ZADANIE DOMOWE 4 DZIEN

--- Pracazaliczeniowa/Pracazaliczeniowa/Program.cs
+++ Pracazaliczeniowa/Pracazaliczeniowa/Program.cs
@@ -1,9 +1,37 @@
 ﻿// Zadanie domowe dzien 4 
 
-using System.Runtime;
+
+using System.ComponentModel.Design;
 
 var name = "Ewa";
-var myAge = 33;
-var result =  (name + myAge);
-Console.WriteLine(result);
+bool woman = true;
+int age= 25;
+
+if (woman = true && age < 30)
+{
+    if (woman = true)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    if (woman == true && age < 35)
+    {
+
+
+        Console.WriteLine("Niepelnoletni Meszczyzna");
+    }
+
+    if (woman == true && age < 32) ;
+    {
+        Console.WriteLine("kobieta ponizej 30 lat");
+    }
+}
+ else if (woman = true && age <= 31) ;
+
+       Console.WriteLine(" Waclaw Skaza ,lat 40 ");
+
+
+       if (woman = true && age <= 31) ;
+
+        Console.WriteLine("Dziekuje za lekcje 4");
+
 
